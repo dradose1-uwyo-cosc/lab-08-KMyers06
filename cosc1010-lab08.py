663fd182b0eb23263e7fcd45d61ca75eb82f95f7
KMyers06 <kmyers20@uwyo.edu>
2024-11-07 18:43:31 +0000

    Finish?

--- cosc1010-lab08.py
+++ cosc1010-lab08.py
@@ -104,3 +104,45 @@ print("*" * 75)
 # Create a loop like above to prompt the user for input for the three values
 # Create a second function that just does the square root operation 
     # If the number you are trying to take the square root of is negative, return null
+def quad_formula(e,f,g):
+    un_root=f**2-4*e*g
+    root=un_root**1/2
+    x1top=-f+root
+    x2top=-f-root
+    bot=2*e
+    x1=x1top/bot
+    x2=x2top/bot
+    print(f"Results: {x1}, {x2}")
+    
+    
+
+while True:
+    nums=input("Input A,B,C for Quad Formula (A B C)=")
+    if nums.lower()=='exit':
+        break
+    a=0
+    b=0
+    c=0
+    num=nums.split()
+    if len(num) != 3:
+        print('Wrong Number of Inputs')
+        break
+    aing=num[0]
+    bing=num[1]
+    cing=num[2]
+    if check_numbers(aing) != False:
+        a=check_numbers(aing)
+    else:
+        print("invalid Input")
+        break 
+    if check_numbers(bing) != False:
+        b=check_numbers(bing)
+    else:
+        print("invalid Input")
+        break 
+    if check_numbers(cing) != False:
+        c=check_numbers(cing)
+    else:
+        print("invalid Input")
+        break 
+    quad_formula(a,b,c)
